7deaba11e7e27d73d9ee3f31705b3d2aecb98263
ole101 <oleh69250@gmail.com>
2025-03-22 01:02:06 +0100

    feat: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.file.DuplicatesStrategy.INCLUDE
+
+plugins {
+    java
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.paperweight.userdev)
+    alias(libs.plugins.run.paper)
+}
+
+group = "de.ole101.marketplace"
+version = "1.0.0-SNAPSHOT"
+description = "An Item Marketplace Plugin."
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    compileOnly(libs.annotations)
+
+    implementation(libs.gson)
+    implementation(libs.guice)
+    implementation(libs.mongodb)
+
+    paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    named<ShadowJar>("shadowJar") {
+        archiveFileName.set("${project.name}.jar")
+    }
+
+    build {
+        dependsOn(named("shadowJar"))
+    }
+
+    processResources {
+        from(sourceSets.main.get().resources.srcDirs) {
+            filesMatching("paper-plugin.yml") {
+                expand("version" to project.version)
+            }
+            duplicatesStrategy = INCLUDE
+        }
+    }
+
+    runServer {
+        minecraftVersion("1.21.4")
+    }
+}
